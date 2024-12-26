4cdbab30a3f8bd06d2544e81e4cc5f25a8f1576f
Arilsamsi <arilsamsi2@gmail.com>
2024-12-26 22:55:02 +0800

    portofolio eyissss

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+    });
+});
